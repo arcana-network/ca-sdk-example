174f0fadd5bd704b1e6a098265da1f11da035743
svarcoder <charvai1997@gmail.com>
2024-12-27 15:05:40 +0530

    Chore: Version Update 30

--- src/types/chainTypes.ts
+++ src/types/chainTypes.ts
@@ -1,38 +1,36 @@
 type TokenInfo = {
-  contractAddress: `0x${string}`
-  name: string
-  symbol: string
-  decimals: number
-  logo?: string
-  platform?: string
-}
+  contractAddress: `0x${string}`;
+  name: string;
+  symbol: string;
+  decimals: number;
+  logo?: string;
+  platform?: string;
+};
 
 type Chain = {
-  id: number
+  id: number;
   rpcUrls: {
     default: {
-      http: string[]
-      publicHttp?: string[]
-      webSocket?: string[]
-    }
-  }
-  name: string
+      publicHttp?: string[];
+    };
+  };
+  name: string;
   blockExplorers?: {
     default: {
-      name: string
-      url: string
-    }
-  }
+      name: string;
+      url: string;
+    };
+  };
   nativeCurrency: {
-    name: string
-    symbol: string
-    decimals: number
-  }
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
   custom: {
-    icon?: string
-    knownTokens: TokenInfo[]
-    verifyingContract?: `0x${string}`
-  }
-}
+    icon?: string;
+    knownTokens: TokenInfo[];
+    verifyingContract?: `0x${string}`;
+  };
+};
 
-export type { Chain }
+export type { Chain };
