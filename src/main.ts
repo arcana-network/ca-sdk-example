e55c4ef5eeff0eaf7d2ee81ea34c2280af334c59
shrinathprabhu <shrinathprabhu.123@gmail.com>
2024-12-27 16:07:00 +0530

    Update the fonts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { store } from '@/stores/index'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { store } from "@/stores/index";
 
-const app = createApp(App)
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 
-app
-  .use(store)
-  .mount('#app')
+const app = createApp(App);
 
+app.use(store).mount("#app");
